9f8a9a1a0bd99e55080ae73441b9c8b4b7918d91
bluebaney10 <bluebaney10@gmail.com>
2025-08-23 18:56:02 +0700

    Creating an API for Summarizing Reviews

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,7 +1,6 @@
-import express from 'express';
 import type { Request, Response } from 'express';
+import express from 'express';
 import { chatController } from './controllers/chat.controller';
-import { PrismaClient } from './generated/prisma';
 import { reviewController } from './controllers/review.controller';
 
 const router = express.Router();
@@ -17,5 +16,9 @@ router.get('/api/hello', (req: Request, res: Response) => {
 router.post('/api/chat', chatController.sendMessage);
 
 router.get('/api/products/:id/reviews', reviewController.getReviews);
+router.post(
+   '/api/products/:id/reviews/summarize',
+   reviewController.summarizeReviews
+);
 
 export default router;
--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,8 +1,17 @@
-import { type Review } from '../generated/prisma';
+import { PrismaClient, type Review } from '../generated/prisma';
 import { reviewRepository } from '../repositories/review.repository';
 
 export const reviewService = {
    async getReviews(productId: number): Promise<Review[]> {
       return reviewRepository.getReviews(productId);
+   },
+   async summarizeReviews(productId: number): Promise<string> {
+      //Get the last 10 reviews
+      const reviews = await reviewRepository.getReviews(productId, 10);
+      const joinedReviews = reviews.map((r) => r.content).join('\n\n');
+
+      //Send the reviews to a LLM
+      const summary = 'This is a placeholder summary';
+      return summary;
    },
 };
