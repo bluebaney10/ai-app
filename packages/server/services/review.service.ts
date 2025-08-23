6b6f3db7fedae5337f91740f16e501585b4ee5c0
bluebaney10 <bluebaney10@gmail.com>
2025-08-23 19:14:06 +0700

    Generating Summaries

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, type Review } from '../generated/prisma';
+import OpenAI from 'openai';
+import { type Review } from '../generated/prisma';
 import { reviewRepository } from '../repositories/review.repository';
 
 export const reviewService = {
@@ -6,12 +7,25 @@ export const reviewService = {
       return reviewRepository.getReviews(productId);
    },
    async summarizeReviews(productId: number): Promise<string> {
-      //Get the last 10 reviews
       const reviews = await reviewRepository.getReviews(productId, 10);
       const joinedReviews = reviews.map((r) => r.content).join('\n\n');
 
-      //Send the reviews to a LLM
-      const summary = 'This is a placeholder summary';
-      return summary;
+      const prompt = `Summarize the following customer reviews into a short paragraph
+      hilighting key themes, both positive and negative:
+       ${joinedReviews}
+      `;
+
+      const response = await client.responses.create({
+         model: 'gpt-4.1',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 500,
+      });
+
+      return response.output_text;
    },
 };
+
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
